eea7fc016d73d3e0ee39dffc1960c6b3ff7377fa
CamilaDamasio <camiladamasioef@hotmail.com>
2021-12-11 16:42:54 -0300

    Cria esqueleto do front

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import EventShow from './pages/EventShow';
+import EventRegister from './pages/EventRegister';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Switch>
+        <Route exact path="/events/register" component={ EventRegister } />
+        <Route exact path="/events/:id" component={ EventShow } />
+        <Route exact path="/" component={ Home } />
+      </Switch>
   );
 }
 
