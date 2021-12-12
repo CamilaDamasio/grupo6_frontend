9ff69543d1e5270450aa1badf3281a1328fdf1e3
CamilaDamasio <camiladamasioef@hotmail.com>
2021-12-12 12:37:48 -0300

    Altera importacao de switch no app.js para routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
-import EventShow from './pages/EventShow';
+import EventShow from './pages/EventsShow';
 import EventRegister from './pages/EventRegister';
-
 
 function App() {
   return (
-      <Switch>
+      <Routes>
         <Route exact path="/events/register" component={ EventRegister } />
         <Route exact path="/events/:id" component={ EventShow } />
         <Route exact path="/" component={ Home } />
-      </Switch>
+      </Routes>
   );
 }
 
